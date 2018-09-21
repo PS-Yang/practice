4395b0d0c8b278d8e9cd24b048cac0dd6d202dca
PS-Yang <ian0204456@hotmail.com>
2018-09-21 10:10:43 +0800

    32

--- practice/src/practice/Solution32.java
+++ practice/src/practice/Solution32.java
@@ -29,49 +29,4 @@ public class Solution32 {
 		}
 		return max;
     }
-	
-//	public int longestValidParentheses(String s) {
-//        int max=0;
-//        Stack<String> stack=new Stack<String>();
-//        int temp=0;
-//        boolean flag=true;
-//        int index=-1;
-//		for(int i=0;i<s.length();i++)
-//		{
-//			String str=s.charAt(i)+"";
-//			if(!stack.empty()  && stack.peek().equals("(") && str.equals(")"))
-//			{
-//				temp=temp+2;
-//				stack.pop();
-//			}
-//			else 
-//			{
-//				stack.push(str);
-//				if(str.equals(")"))
-//					temp=0;
-//			}
-//			if(temp>max)
-//				max=temp;
-//		}
-//		return max;
-//    }
-	
-	
-//	 public int longestValidParentheses(String s) {
-//	        Stack<Integer> stack = new Stack<Integer>();
-//	        int max=0;
-//	        int left = -1;
-//	        for(int j=0;j<s.length();j++){
-//	            if(s.charAt(j)=='(') stack.push(j);            
-//	            else {
-//	                if (stack.isEmpty()) left=j;
-//	                else{
-//	                    stack.pop();
-//	                    if(stack.isEmpty()) max=Math.max(max,j-left);
-//	                    else max=Math.max(max,j-stack.peek());
-//	                }
-//	            }
-//	        }
-//	        return max;
-//	    }
 }
