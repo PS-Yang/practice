b074b89bbe3f5c43b7d101a3567e94060a67fbb3
PS-Yang <ian0204456@hotmail.com>
2018-10-05 18:14:42 +0800

    35

--- practice/src/practice/Solution35.java
+++ practice/src/practice/Solution35.java
@@ -21,8 +21,21 @@ public class Solution35 {
 				ans=begin;
 			}
 		}
-		if(ans==-1)
-			return 0;
 		return ans;
     }
+//	public int searchInsert(int[] nums, int target) {
+//		int begin=0;
+//		int end=nums.length-1;
+//		while(begin<=end)
+//		{
+//			int mid=(begin+end)/2;
+//			if(nums[mid]==target)
+//				return mid;
+//			else if(nums[mid]>target)
+//				end=mid-1;
+//			else
+//				begin=mid+1;
+//		}
+//		return begin;
+//    }
 }
