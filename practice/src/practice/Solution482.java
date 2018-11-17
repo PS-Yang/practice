040c9edd0a9fa1bee23487577b149f24b67be581
PS-Yang <ian0204456@hotmail.com>
2018-11-18 00:15:42 +0800

    482

---
+++ practice/src/practice/Solution482.java
@@ -0,0 +1,21 @@
+package practice;
+
+public class Solution482 {
+	
+	//sol 1
+	public String licenseKeyFormatting(String S, int K) {
+		StringBuilder sb=new StringBuilder();
+		S=S.toUpperCase().replaceAll("-", "");
+		int len=S.length();
+		int first=len%K;
+		int prv=0;
+		for(int i=first;i<=S.length();i=i+K)
+		{
+			sb.append(S.substring(prv,i));
+			if(i!=S.length() && i!=0)
+				sb.append("-");
+			prv=i;
+		}
+		return sb.toString();
+    }
+}
