15d15ac401b60f05f7afaaecc72c0c0237c480a5
PS-Yang <ian0204456@hotmail.com>
2018-11-26 12:48:14 +0800

    945 948 fixed

--- practice/src/practice/Solution945.java
+++ practice/src/practice/Solution945.java
@@ -1,39 +1,67 @@
 package practice;
 
+import java.util.Arrays;
 import java.util.HashSet;
 
 public class Solution945 {
+	//sol3 O(N)
+	public int minIncrementForUnique(int[] A) {
+		if(A.length==0)
+			return 0;
+		int num[]=new int [80001];
+		for(int n:A)
+			num[n]++;
+		int count=0;
+		int take=0;
+		for(int i=0;i<80000;i++)
+		{
+			if(num[i]>1)
+			{
+				take=take+num[i]-1;
+				count=count-i*(num[i]-1);
+			}
+			else if(take>0 && num[i]==0)
+			{
+				take--;
+				count=count+i;
+			}
+				
+		}
+		return count;
+	}
+	//sol2 time complete O(NlogN)
+//	public int minIncrementForUnique(int[] A) {
+//		if(A.length==0)
+//			return 0;
+//		Arrays.sort(A);
+//		int count=0;
+//		int cur=A[0]+1;
+//		for(int i=1;i<A.length;i++)
+//		{
+//			if(cur<A[i])
+//				cur=A[i];
+//			else
+//				count=count+cur-A[i];
+//			cur++;
+//		}
+//		return count;
+//	}
+	
+	//sol1 in contest => time O(N*N)
 //	public int minIncrementForUnique(int[] A) {
 //		if(A.length==0)
 //			return 0;
 //		int count=0;
-//		HashSet<Integer> set=new HashSet<>();
-//		
-//		for(int i=0;i<A.length;i++)
+//		int num[]=new int [80001];
+//		num[A[0]]=1;
+//		for(int i=1;i<A.length;i++)
 //		{
 //			int tmp=0;
-//			while(set.contains(tmp+A[i])) 
+//			while(num[A[i]+tmp]==1) 
 //				tmp++;
-//			set.add(A[i]+tmp);
+//			num[A[i]+tmp]=1;
 //			count=count+tmp;
 //		}
 //		return count;
 //	}
-	//sol1
-	public int minIncrementForUnique(int[] A) {
-		if(A.length==0)
-			return 0;
-		int count=0;
-		int num[]=new int [80001];
-		num[A[0]]=1;
-		for(int i=1;i<A.length;i++)
-		{
-			int tmp=0;
-			while(num[A[i]+tmp]==1) 
-				tmp++;
-			num[A[i]+tmp]=1;
-			count=count+tmp;
-		}
-		return count;
-	}
 }
--- practice/src/practice/Solution948.java
+++ practice/src/practice/Solution948.java
@@ -3,6 +3,7 @@ package practice;
 import java.util.Arrays;
 
 public class Solution948 {
+	//sol2 fixed.
 	public int bagOfTokensScore(int[] tokens, int P) {
 		if(tokens.length==0)
 			return 0;
@@ -12,21 +13,46 @@ public class Solution948 {
 		int j=tokens.length-1;
 		while(i<=j && P>tokens[i])
 		{
-			while(i<tokens.length && P>=tokens[i])
+			while(i<=j && P>=tokens[i])
 			{
-				P=P-tokens[i];
-				i++;
+				P=P-tokens[i++];
 				ans++;
 			}
 			if(i>j)
 				break;
 			if(ans>0 && i!=j && (P+tokens[j]) > tokens[i])
 			{
-				P=P+tokens[j];
+				P=P+tokens[j--];
 				ans--;
-				j--;
 			}
 		}
 		return ans;
     }
+	//sol1 
+//	public int bagOfTokensScore(int[] tokens, int P) {
+//		if(tokens.length==0)
+//			return 0;
+//		int ans=0;
+//		Arrays.sort(tokens);
+//		int i=0;
+//		int j=tokens.length-1;
+//		while(i<=j && P>tokens[i])
+//		{
+//			while(i<tokens.length && P>=tokens[i])
+//			{
+//				P=P-tokens[i];
+//				i++;
+//				ans++;
+//			}
+//			if(i>j)
+//				break;
+//			if(ans>0 && i!=j && (P+tokens[j]) > tokens[i])
+//			{
+//				P=P+tokens[j];
+//				ans--;
+//				j--;
+//			}
+//		}
+//		return ans;
+//    }
 }
